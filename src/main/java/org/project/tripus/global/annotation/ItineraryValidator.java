d8cc4d434de56d0f962b4762b7d9faf8e73ac917
Kim Hyunji <hzunhy@gmail.com>
2025-06-23 17:17:44 +0900

    Fix: @ValidItinerary 어노테이션 수정

--- src/main/java/org/project/tripus/global/annotation/ItineraryValidator.java
+++ src/main/java/org/project/tripus/global/annotation/ItineraryValidator.java
@@ -11,17 +11,13 @@ public class ItineraryValidator implements ConstraintValidator<ValidItinerary, L
     public boolean isValid(List<List<SaveTripPlaceItemRequestDto>> itinerary, ConstraintValidatorContext context) {
         for(List<SaveTripPlaceItemRequestDto> placeList : itinerary) {
             for(SaveTripPlaceItemRequestDto place : placeList) {
-                if(isBlank(place.getContentid()) || isBlank(place.getContenttypeid()) || isBlank(place.getTitle())) {
+                if(place.getContentid().isBlank() || place.getContenttypeid().isBlank() || place.getTitle().isBlank()) {
                     return false;
                 }
             }
         }
 
         return true;
-    }
-
-    private boolean isBlank(String str) {
-        return str == null || str.trim().isEmpty();
     }
 
     @Override
