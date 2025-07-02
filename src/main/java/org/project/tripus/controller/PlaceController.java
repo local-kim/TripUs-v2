71b3ee45d67cd4e8ef78ace285839e6813edfd7b
Kim Hyunji <hzunhy@gmail.com>
2025-07-02 12:20:32 +0900

    Chore: URL 변경

--- src/main/java/org/project/tripus/controller/PlaceController.java
+++ src/main/java/org/project/tripus/controller/PlaceController.java
@@ -18,11 +18,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @Tag(name = "장소 API", description = "장소, 장소 좋아요")
+@RequestMapping("/place")
 @RestController
 public class PlaceController {
 
@@ -36,7 +38,7 @@ public class PlaceController {
     @Operation(summary = "좋아요 누른 장소 목록 조회", description = "현재 로그인한 회원이 특정 도시에서 좋아요 누른 장소의 목록을 조회합니다.")
     @SecurityRequirement(name = "JWT")
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/liked-place")
+    @GetMapping("/liked")
     public ResponseEntity<?> getLikedPlaceList(
         @RequestParam @NotNull Long cityId,
         @AuthenticationPrincipal CustomUserDetails customUserDetails
